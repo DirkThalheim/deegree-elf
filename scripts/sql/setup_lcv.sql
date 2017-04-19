8e54fad9ac909b874211daa8ab6583ab73aa2dfb
Dirk Thalheim <dirk.thalheim@bkg.bund.de>
2017-04-19 14:52:39 +0200

    LCV Updates
    finalization of FeatureTypeMapping and SQL for LCV

--- scripts/sql/setup_lcv.sql
+++ scripts/sql/setup_lcv.sql
@@ -8,25 +8,18 @@ COMMENT ON SCHEMA lcv IS 'Schemata für Landcover';
 
 -- == Tabellen LandcoverDataset =======
 CREATE TABLE lcv.landcoverdataset (
-    id text,
-    identifier text,
-    identifier_codespace text,
-    identifier_localid text,
-    identifier_namespace text,
-    identifier_versionid text,
-    identifier_versionid_nilreason text,
-    identifier_versionid_xsi_nil boolean,
+    localid text,
     beginlifespanversion timestamp,
     beginlifespanversion_nilreason text,
-    beginlifespanversion_xsi_nil boolean,
+    beginlifespanversion_nil boolean,
     name text,
     validfrom date,
     validfrom_nilreason text,
-    validfrom_xsi_nil boolean,
+    validfrom_nil boolean,
     validto date,
     validto_nilreason text,
-    validto_xsi_nil boolean,
-    CONSTRAINT landcoverdataset_pkey PRIMARY KEY (id)
+    validto_nil boolean,
+    CONSTRAINT landcoverdataset_pkey PRIMARY KEY (localid)
 );
 
 CREATE TABLE lcv.landcoverdataset_member (
@@ -43,27 +36,15 @@ CREATE TABLE lcv.landcoverdataset_member (
 ALTER TABLE lcv.landcoverdataset OWNER TO elf_admin;
 ALTER TABLE lcv.landcoverdataset_member OWNER TO elf_admin;
 
-CREATE INDEX landcoverdataset_id_idx ON lcv.landcoverdataset(id);
-CREATE INDEX landcoverdataset_member_id_idx ON lcv.landcoverdataset_member(id);
-CREATE INDEX landcoverdataset_member_parentfk_idx ON lcv.landcoverdataset_member(parentfk);
-
 -- == Tabellen LandcoverUnit =======
 CREATE TABLE lcv.landcoverunit (
-    id text,
-    identifier text,
-    identifier_codespace text,
-    identifier_localid text,
-    identifier_namespace text,
-    identifier_versionid text,
-    identifier_versionid_nilreason text,
-    identifier_versionid_xsi_nil boolean,
+    localid text,
     beginlifespanversion timestamp,
     beginlifespanversion_nilreason text,
-    beginlifespanversion_xsi_nil boolean,
+    beginlifespanversion_nil boolean,
     geometry_nilreason text,
     geometry_remoteschema text,
-    geometry_owns boolean,
-    CONSTRAINT landcoverunit_pkey PRIMARY KEY (id)
+    CONSTRAINT landcoverunit_pkey PRIMARY KEY (localid)
 );
 SELECT ADDGEOMETRYCOLUMN('lcv', 'landcoverunit','geometry','4258','GEOMETRY', 2);
 
@@ -78,7 +59,7 @@ CREATE TABLE lcv.landcoverunit_landcoverobservation (
     class_href text,
     observationdate timestamp,
     observationdate_nilreason text,
-    observationdate_xsi_nil boolean
+    observationdate_nil boolean
 );
 CREATE TABLE lcv.landcoverunit_mosaic (
     id serial PRIMARY KEY,
@@ -93,15 +74,11 @@ CREATE TABLE lcv.landcoverunit_mosaic (
     landcovervalue_class_href text,
     landcovervalue_coveredpercentage integer,
     landcovervalue_coveredpercentage_nilreason text,
-    landcovervalue_coveredpercentage_xsi_nil boolean
+    landcovervalue_coveredpercentage_nil boolean
 );
 
 ALTER TABLE lcv.landcoverunit OWNER TO elf_admin;
 ALTER TABLE lcv.landcoverunit_landcoverobservation OWNER TO elf_admin;
 ALTER TABLE lcv.landcoverunit_mosaic OWNER TO elf_admin;
 
-CREATE INDEX landcoverunit_id_idx ON lcv.landcoverunit(id);
 CREATE INDEX landcoverunit_gemotry_idx ON lcv.landcoverunit USING GIST (geometry);
-
-CREATE INDEX landcoverunit_landcoverobservation_id_idx ON lcv.landcoverunit_landcoverobservation(id);
-CREATE INDEX landcoverunit_landcoverobservation_parentfk_idx ON lcv.landcoverunit_landcoverobservation(parentfk);
